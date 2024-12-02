a2136e1b4eb1477fbad5198ce16758df6524d6c8
RAPEGOLI <rodrigo.pegoli@outlook.com>
2024-12-02 10:02:05 -0300

    Aprendi o que são variáveis
    Inclusive sobre prints!

---
+++ Aula 02/aula02.py
@@ -0,0 +1,11 @@
+print("lembre de mim")
+print("coco")
+print("hector")
+print("good morning to everyone")
+print("chocolate with pepper")
+print("somewhere over the rainbow")
+nome_usuario = "Rafael" #tipo -> str = string
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.77 # tipo -> float
+maior_de_idade = False # tipo -> bool = boolean
+print("Seja bem-vindo", nome_usuario, "sua idade é", idade_usuario, "sua altura é", altura_usuario, "você é maior de idade?", maior_de_idade)
